239fee5e6d380cd8da5fea79c21faac575d5764f
Santiago Negreira <sansfleitas@gmail.com>
2024-08-20 18:42:57 -0300

    Initialize Task CLI project

---
+++ src/main/java/com/negreira/santiago/task_cli/TaskCliApplication.java
@@ -0,0 +1,13 @@
+package com.negreira.santiago.task_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/negreira/santiago/task_cli/TaskCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.negreira.santiago.task_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
