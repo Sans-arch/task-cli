5270cd9f609672f0d3b3843f018a89fed071cda1
Santiago Negreira <sansfleitas@gmail.com>
2024-08-20 22:19:11 -0300

    Remove RepositoyInterface (domain) and TaskRepositoryInterface (domain)

--- src/main/java/com/negreira/santiago/task_cli/domain/repository/RepositoryInterface.java
+++
@@ -1,9 +0,0 @@
-package com.negreira.santiago.task_cli.domain.repository;
-
-import java.util.List;
-
-public interface RepositoryInterface<T> {
-    void save(T task);
-    T get();
-    List<T> findAll();
-}
--- src/main/java/com/negreira/santiago/task_cli/domain/repository/TaskRepositoryInterface.java
+++
@@ -1,6 +0,0 @@
-package com.negreira.santiago.task_cli.domain.repository;
-
-import com.negreira.santiago.task_cli.domain.entity.Task;
-
-public interface TaskRepositoryInterface extends RepositoryInterface<Task> {
-}
